cfb66e7946cae212ca228bd3cd767e64862f4d20
drdLock <blackburton23@gmail.com>
2021-01-21 21:45:42 -0800

    Added basic language feature to calculator

--- rb101/2_lesson/calculator/calculator.rb
+++ rb101/2_lesson/calculator/calculator.rb
@@ -1,6 +1,12 @@
 # Simple Arithmetic!
 require "pry"
+require 'yaml'
 
+MESSAGES = YAML.load_file('calc_messages.yml')
+
+def messages(messages, lang='en')
+  MESSAGES[lang][messages]
+end
 
 def prompt(message)
   puts "=> #{message}"
@@ -8,7 +14,8 @@ end
 
 def op_assign
   begin
-    prompt("Enter an arithmetic operation ('+', '-', '*', '/') or 'q' to quit")
+    #prompt("Enter an arithmetic operation ('+', '-', '*', '/') or 'q' to quit")
+    prompt(messages('op_title', lang))
     op = gets.chomp
     raise
   rescue
@@ -17,9 +24,11 @@ def op_assign
   op
 end
 
-def num_assign(number)
+
+def num_assign(number, lang)
   begin
-    prompt("Enter your #{number} number or 'q' to quit")
+    #prompt("Enter your #{number} number or 'q' to quit")
+    prompt(messages('num_title', lang))
     num = gets.chomp
     raise if num.to_f == 0.0
   rescue
@@ -28,33 +37,42 @@ def num_assign(number)
   num
 end
 
+
 loop do
   # binding.pry #execution will stop here if uncommented
-  prompt('Simple Arithmetic!')
+  #prompt('Simple Arithmetic!')
+  
+  puts "...for english press 1, para espanol oprima 2..." 
+  lang_choice = gets.chomp.to_i
 
-  first_num = num_assign('first')
+  next if lang_choice != 1 && lang_choice != 2
+
+  lang_choice == 1 ? lang = 'en' : lang = 'es'
+
+  prompt(messages('welcome', lang))
+  first_num = num_assign('first', lang)
   first_num == 'q' ? break : first_num = first_num.to_f
 
   prompt(first_num.to_s)
 
-  op = op_assign
+  op = op_assign(lang)
   break if op == 'q'
 
   prompt("#{first_num} #{op}")
 
-  second_num = num_assign('second')
+  second_num = num_assign('second', lang)
   second_num == 'q' ? break : second_num = second_num.to_f
 
   answer =
     case op
     when '+'
-      prompt("#{first_num} plus #{second_num} equals #{first_num + second_num}")
+      prompt("#{first_num} + #{second_num} = #{first_num + second_num}")
     when '-'
-      prompt("#{first_num} minus #{second_num} equals #{first_num - second_num}")
+      prompt("#{first_num} - #{second_num} = #{first_num - second_num}")
     when '*'
-      prompt("#{first_num} times #{second_num} equals #{first_num * second_num}")
+      prompt("#{first_num} * #{second_num} = #{first_num * second_num}")
     when '/'
-      prompt("#{first_num} divided by #{second_num} equals #{first_num / second_num}")
+      prompt("#{first_num} / #{second_num} = #{first_num / second_num}")
     end
 
   puts answer
